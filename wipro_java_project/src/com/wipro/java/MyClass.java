7cc7578fbca3e55cf8203fd96353450f069b8dc7
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-01 17:26:53 +0530

    day1

---
+++ wipro_java_project/src/com/wipro/java/MyClass.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.wipro.java;
+
+/**
+ * 
+ */
+public class MyClass {
+    int num = 5;
+
+    public void changeValue(int num) {
+        this.num = num;
+    }
+
+    public static void main(String[] args) {
+        MyClass obj = new MyClass();
+        obj.changeValue(10);
+        System.out.println(obj.num);
+    }
+}
