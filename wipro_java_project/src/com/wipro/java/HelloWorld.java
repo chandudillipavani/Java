7df21f3d0c4a036e6edbe5a71b3021e32f137868
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-01 16:06:08 +0530

    commit

--- wipro_java_project/src/com/wipro/java/HelloWorld.java
+++ wipro_java_project/src/com/wipro/java/HelloWorld.java
@@ -21,7 +21,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("this is java program");
-		System.out.println("my name is pavani");
+		System.out.println("my name is pavani");	
 		System.out.println("my skills are java");
 		
 
