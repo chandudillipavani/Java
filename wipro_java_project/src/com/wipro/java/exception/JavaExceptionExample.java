c751258c809700390b5368ba52b9ba60a91f7faf
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-04 15:20:10 +0530

    Day3

--- wipro_java_project/src/com/wipro/java/exception/JavaExceptionExample.java
+++ wipro_java_project/src/com/wipro/java/exception/JavaExceptionExample.java
@@ -1,16 +1,28 @@
 package com.wipro.java.exception;
 
 public class JavaExceptionExample {
-    public static void main(String[] args) {
-        int[] numbers = {10, 20, 30, 40, 50};
-        
+    public static void main(String args[]) {
+        int num1 = 100, num2 = 0, result;
+        int[] arr = new int[3]; // Array of size 3
+
         try {
-            System.out.println("Accessing element: " + numbers[5]); // This will cause ArrayIndexOutOfBoundsException
+            // Attempting division by zero
+            result = num1 / num2;
+            System.out.println("Result: " + result);
+
+            // Attempting to access an invalid array index
+            arr[5] = 50; // This will cause an ArrayIndexOutOfBoundsException
         } 
-        catch (ArrayIndexOutOfBoundsException e) { 
-            System.out.println("Exception caught: " + e);
+        catch (ArithmeticException e) {
+            System.out.println("ArithmeticException caught: " + e);
+        } 
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println("ArrayIndexOutOfBoundsException caught: " + e);
+        } 
+        finally {
+            System.out.println("executed");
         }
-        
-        System.out.println("Rest of the code executes normally.");
+
+        System.out.println("Rest of the code...");
     }
 }
