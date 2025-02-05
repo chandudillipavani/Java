4421735576dde35ff18bdd466a6c3f8593b267c8
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-05 14:11:33 +0530

    Day4

---
+++ wipro_java_project/src/com/wipro/java/collection/Array.java
@@ -0,0 +1,20 @@
+package com.wipro.java.collection;
+
+public class Array {
+	 public static void main(String[] args) {
+	        // Creating an array of integers
+	        int[] numbers = new int[5]; // Array with 5 elements
+
+	        // Assigning values
+	        numbers[0] = 10;
+	        numbers[1] = 20;
+	        numbers[2] = 30;
+	        numbers[3] = 40;
+	        numbers[4] = 50;
+
+	        // Printing the array
+	        for (int i = 0; i < numbers.length; i++) {
+	            System.out.println(numbers[i]);
+	        }
+	    }
+}
