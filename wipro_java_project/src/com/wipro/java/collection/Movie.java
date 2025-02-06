79ac8e2f7eb12b6ae10bf2d66947615270736723
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-06 10:32:12 +0530

    Day5

--- wipro_java_project/src/com/wipro/java/collection/Movie.java
+++ wipro_java_project/src/com/wipro/java/collection/Movie.java
@@ -31,21 +31,4 @@ class Movie implements Comparable<Movie> {
     public int getYear() {
         return y;
     }
-
-    public static void main(String[] args) {
-        // Create a list of movies
-        ArrayList<Movie> movies = new ArrayList<>();
-        movies.add(new Movie("Star Wars", 8.7, 1977));
-        movies.add(new Movie("Return of the Jedi", 8.4, 1983));
-        movies.add(new Movie("Empire Strikes Back", 8.8, 1980));
-
-        // Sort movies by rating using Comparable's compareTo method
-        Collections.sort(movies);
-
-        // Display sorted list
-        System.out.println("Movies after sorting by rating (Ascending order):");
-        for (Movie m : movies) {
-            System.out.println(m.getName() + " " + m.getRating() + " " + m.getYear());
-        }
-    }
 }
