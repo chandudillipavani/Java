fb840004463ed88799e35bf92659ed98c39f0819
chandu dilli pavani <dillipavanichandu1@gmail.com>
2025-02-07 12:20:30 +0530

    Day6

---
+++ wipro_java_project/src/com/wipro/java/junit/CalculatorTest.java
@@ -0,0 +1,34 @@
+package com.wipro.java.junit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class CalculatorTest {
+    
+    private final Calculator calculator = new Calculator();
+
+    @Test
+    public void testAdd() {
+        int result = calculator.add(3, 2);
+        assertEquals(5, result);
+    }
+
+    @Test
+    public void testSubtract() {
+        int result = calculator.subtract(5, 3);
+        assertEquals(2, result);
+    }
+
+    
+    public void testAddWithNegativeNumbers() {
+        int result = calculator.add(-3, -2);
+        assertEquals(-5, result);
+    }
+
+    
+    public void testSubtractWithNegativeNumbers() {
+        int result = calculator.subtract(-3, -2);
+        assertEquals(-1, result);
+    }
+}
